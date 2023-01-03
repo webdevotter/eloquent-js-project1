0c2cf489e18989d7ea9e0512ef1ce1321496920c
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2023-01-03 10:53:10 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# eloquent-js-project1
+ 
---
+++ triangle.js
@@ -0,0 +1,5 @@
+for (let hash = "#"; hash.length <= 7; hash += "#") {
+  console.log(hash);
+}
+
+// Source: Eloquent JavaScript Third Edition "A Modern Introduction to Programming" Marijn Haverbeke
